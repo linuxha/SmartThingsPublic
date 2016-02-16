20748913c54c25218e344c7500b37dc441846288
Neil Cherry <ncherry@linuxha.com>
2016-02-15 19:39:56 -0500

    Changed settings

--- smartapps/smartthings/web-services-tutorial.src/web-services-tutorial.groovy
+++ smartapps/smartthings/web-services-tutorial.src/web-services-tutorial.groovy
@@ -18,7 +18,7 @@ definition(
     namespace: "smartthings",
     author: "SmartThings",
     description: "web services tutorial",
-    category: "",
+    category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
