7b186a8500932692d406f15fb56b05a95335867c
Neil Cherry <ncherry@linuxha.com>
2016-02-21 19:50:12 -0500

    Added on, off and level to details

--- devicetypes/linuxha/vdimmer.src/vdimmer.groovy
+++ devicetypes/linuxha/vdimmer.src/vdimmer.groovy
@@ -55,7 +55,7 @@ metadata {
 			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
 		main "switch"
-		details(["switch","lValue","refresh"])
+		details(["switch","on","off","level","lValue","refresh"])
 
 	}
 }
